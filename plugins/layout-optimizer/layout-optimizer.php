f5417f197e6baa734cae6e1454a68fd5846b7b9a
kkanazaw <new.frontier2006@gmail.com>
2019-06-26 11:41:37 +0900

    fixed #1 開発環境の構築

---
+++ plugins/layout-optimizer/layout-optimizer.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: LayoutOptimizer
+  Plugin URI:
+  Description: レイアウトを最適化するプラグイン
+  Version: 1.0.0
+  Author: designrule
+  Author URI: https://github.com/designrule/layout-optimizer-plugin
+  License: UNLICENSED
+ */
+
+?>
