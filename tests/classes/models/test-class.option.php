ceba3f157e4f8617b05bd0609b536df609a52c06
kkanazaw <new.frontier2006@gmail.com>
2019-08-13 19:07:40 +0900

    [WIP]クラス名を修正

--- tests/classes/models/test-class.option.php
+++ tests/classes/models/test-class.option.php
@@ -5,12 +5,12 @@
  * @package Layout_Optimizer
  */
 
-class LayoutOptimizerOptionsTest extends WP_UnitTestCase {
+class LayoutOptimizerOptionTest extends WP_UnitTestCase {
 	/**
 	 * @test
 	 */
 	public function is_api_loginはログインしていない場合falseを返す() {
-		$data = new LayoutOptimizerOptions([]);
+		$data = new LayoutOptimizerOption([]);
 		// Replace this with some actual testing code.
 		$this->assertFalse($data->is_api_login());
 	}
@@ -19,7 +19,7 @@ class LayoutOptimizerOptionsTest extends WP_UnitTestCase {
 	 * @test
 	 */
 	public function is_api_loginはログインしている場合場合trueを返す() {
-		$data = new LayoutOptimizerOptions(["uid" => "1", "client"=>"hoge", "expiry" => 123, "access_token" =>"123abc"]);
+		$data = new LayoutOptimizerOption(["uid" => "1", "client"=>"hoge", "expiry" => 123, "access_token" =>"123abc"]);
 		// Replace this with some actual testing code.
 		$this->assertTrue($data->is_api_login());
 	}
